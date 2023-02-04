2c33c1bba65494f2a747a72d528ed8a3027a056f
Leigh MacDonald <leigh.macdonald@gmail.com>
2023-02-03 21:52:08 -0700

    Ensure non-null.

--- migrations/0001_create_table_player_stats.up.sql
+++ migrations/0001_create_table_player_stats.up.sql
@@ -1,29 +1,25 @@
 create table if not exists player
 (
-    steam_id  integer primary key,
-    kills_on int default 0,
-    deaths_by int default 0,
-    created_on date default (DATETIME('now')),
-    updated_on date default (DATETIME('now'))
+    steam_id integer primary key,
+    kills_on int not null default 0,
+    deaths_by int not null default 0,
+    created_on date not null default (DATETIME('now')),
+    updated_on date not null default (DATETIME('now'))
 );
 
 create table if not exists player_names
 (
     name_id int primary key,
-    steam_id integer,
-    name text,
-    created_on date default (DATETIME('now')),
-    FOREIGN KEY(steam_id) REFERENCES player(steam_id)
+    steam_id integer not null REFERENCES player(steam_id),
+    name text not null,
+    created_on date not null default (DATETIME('now'))
 );
 
 create index if not exists idx_steam_id_name on player_names (steam_id, name);
 
 create table if not exists player_messages (
     message_id int primary key,
-    steam_id integer,
-    message text,
-    created_on date default (DATETIME('now')),
-    FOREIGN KEY(steam_id) REFERENCES player(steam_id)
+    steam_id integer not null REFERENCES player(steam_id),
+    message text not null,
+    created_on date not null default (DATETIME('now'))
 );
-
-create index if not exists idx_steam_id_name on player_messages (steam_id, message);
