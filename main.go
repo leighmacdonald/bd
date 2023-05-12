4e8f18d631ee3f9651b953f68276abe74a23557c
Leigh MacDonald <leigh.macdonald@gmail.com>
2023-05-12 00:38:53 -0600

    Use errgroup to synchronize statup/shutdown of subsystems.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	gerrors "errors"
 	"fmt"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/leighmacdonald/bd/internal/detector"
@@ -11,7 +12,10 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 var (
@@ -23,7 +27,6 @@ var (
 )
 
 func main() {
-	ctx := context.Background()
 	versionInfo := detector.Version{Version: version, Commit: commit, Date: date, BuiltBy: builtBy}
 
 	userSettings, errSettings := detector.NewSettings()
@@ -34,7 +37,6 @@ func main() {
 	if errReadSettings := userSettings.ReadDefaultOrCreate(); errReadSettings != nil {
 		fmt.Printf("Failed to read settings: %v", errReadSettings)
 	}
-
 	userSettings.MustValidate()
 
 	dbPath := userSettings.DBPath()
@@ -51,15 +53,38 @@ func main() {
 
 	detector.Init(versionInfo, userSettings, rootLogger, dataStore, false)
 
+	rootCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	execGroup, grpCtx := errgroup.WithContext(rootCtx)
+	execGroup.Go(func() error {
+		detector.Start(rootCtx)
+		return nil
+	})
 	if userSettings.GetHttpEnabled() {
-		web.Init(detector.Logger(), false)
-		go web.Start(ctx, userSettings.HTTPListenAddr)
+		execGroup.Go(func() error {
+			web.Init(detector.Logger(), false)
+			return web.Start(grpCtx, userSettings.HTTPListenAddr)
+		})
+		execGroup.Go(func() error {
+			<-grpCtx.Done()
+			var err error
+			err = gerrors.Join(err, web.Stop())
+			err = gerrors.Join(err, detector.Shutdown())
+			return err
+		})
 	}
+
 	if userSettings.GetGuiEnabled() {
-		go detector.Start(ctx)
-		ui.Init(ctx, detector.Logger(), versionInfo)
-		ui.Start(ctx) // *must* be called from main goroutine
+		go func() {
+			if errExit := execGroup.Wait(); errExit != nil {
+				fmt.Printf(errExit.Error())
+			}
+		}()
+		ui.Init(rootCtx, detector.Logger(), versionInfo)
+		ui.Start(rootCtx) // *must* be called from main goroutine
 	} else {
-		detector.Start(ctx)
+		if errExit := execGroup.Wait(); errExit != nil {
+			fmt.Printf(errExit.Error())
+		}
 	}
 }
