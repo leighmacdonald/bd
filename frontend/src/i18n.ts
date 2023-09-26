412b0ad0697f56ce2501d2af6942695094f124cf
Leigh MacDonald <leigh.macdonald@gmail.com>
2023-09-26 09:12:24 -0600

    Enable language detector

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 const resources = {
     en: {
@@ -173,7 +174,7 @@ const resources = {
     }
 };
 
-i18n.use(initReactI18next).init({
+i18n.use(LanguageDetector).use(initReactI18next).init({
     resources,
     fallbackLng: 'en',
     //lng: 'en',
