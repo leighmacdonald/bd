dc40888feb9551a2370eab32bf2eb87c23f631ae
Barmem <igor.neim7@gmail.com>
2023-09-29 10:14:15 +0600

    Added russian localization

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -163,16 +163,168 @@ const resources = {
             }
         }
     },
-ru: {
+    ru: {
         common: {
+            appname: 'Bot Detector',
+            button: {
+                save: 'Сохранить',
+                cancel: 'Отмена'
+            },
+            yes: 'да',
+            no: 'нет',
+            player_table: {
+                column: {
+                    user_id: 'uid',
+                    name: 'имя',
+                    score: 'счёт',
+                    kills: 'убийства',
+                    deaths: 'смерти',
+                    kpm: 'у/м',
+                    health: 'здоровье',
+                    connected: 'время',
+                    map_time: 'время карты',
+                    ping: 'пинг'
+                },
+                notes: {
+                    title: 'Изменить Заметки О Игроке',
+                    note_label: 'Заметка'
+                },
+                row: {
+                    icon_dead: 'Игрок мёртв (хах)',
+                    vac_bans: 'Известные VAC баны',
+                    source_bans: 'Известные Sourcebans',
+                    player_on_lists: 'Игрок отмечен в одном или нескольких списках',
+                    player_on_lists_whitelisted:
+                        'Игрок отмечен, но находится в белом списке',
+                    player_notes: 'Есть заметки о игроке'
+                },
+                menu: {
+                    mark_label: 'Отметить Игрока Как',
+                    external_label: 'Открыть Внкшнюю Ссылку',
+                    copy_label: 'Скопировать SteamID',
+                    chat_history_label: 'История Чата',
+                    name_history_label: 'История Имён',
+                    remove_whitelist_label: 'Удалить Из Белого Списка',
+                    whitelist_label: 'Добавить В Белый Список'
+                },
+                details: {
+                    uid_label: 'UID',
+                    name_label: 'Имя',
+                    visibility_label: 'Видимость Профиля',
+                    vac_bans_label: 'Vac Баны',
+                    game_bans_label: 'Игровые Баны',
+                    matches: {
+                        origin_label: 'Источник',
+                        type_label: 'Тип',
+                        tags_label: 'Метка'
+                    },
+                    sourcebans: {
+                        site_name_label: 'Имя Сайта',
+                        created_label: 'Создан',
+                        perm_label: 'Постоянный',
+                        reason_label: 'Причина'
+                    }
+                }
+            },
+            toolbar: {
+                button: {
+                    show_only_negative:
+                        'Отображать только игроков с каким-либо негативным статусом',
+                    shown_columns: 'Настроить отображаемые столбцы',
+                    open_settings: 'Открыть настройки',
+                    game_state_running: 'Игра запущена',
+                    game_state_stopped: 'Запустить TF2!'
+                }
+            },
             settings: {
                 general: {
-                    label: '1st Бармен'
+                    label: 'Общие',
+                    description: 'Kicker, Метки, Предупреждения В Чате',
+                    chat_warnings_label: 'Предупреждения В Чате',
+                    chat_warnings_tooltip:
+                        'Активировать предупреждения в игровом чате в текущей игре',
+                    kicker_enabled_label: 'Kicker Активирован',
+                    kicker_enabled_tooltip:
+                        'Включить функционал автоматического начала голосования',
+                    kick_tags_label: 'Выгоняемые метки',
+                    kick_tags_tooltip:
+                        'Только при совпадениях по этим меткам сработает начало голосования или предупреждение.',
+                    party_warnings_enabled_label: 'Предупреждения Лобби Активированы',
+                    party_warnings_enabled_tooltip:
+                        'Активировать отправку лог сообщений в чат лобби',
+                    discord_presence_enabled_label: 'Discord Presence Активирован',
+                    discord_presence_enabled_tooltip:
+                        'Активировать отображение игрового статуса в твоём дискорд профиле.',
+                    auto_launch_game_label: 'Авто Запуск Игры',
+                    auto_launch_game_tooltip:
+                        'Когда активирован, при запуске bd, TF2 так же будет запущена',
+                    auto_close_on_game_exit_label: 'Авто Закрытие При Выходе Из Игры',
+                    auto_close_on_game_exit_tooltip:
+                        'Когда активирован, при выходе из игры, так же закрывается bd.',
+                    debug_log_enabled_label: 'Активирован Дебаг Лог',
+                    debug_log_enabled_tooltip:
+                        'Когда активирован, логи записываются в bd.log в папку конфигурации приложения'
+                },
+                player_lists: {
+                    label: 'Списки Игроков и Правил',
+                    description: 'Авто-Kicker, метки, Предупреждения в чате'
+                },
+                external_links: {
+                    label: 'Внешние Ссылки',
+                    description: 'Настроить меню кастомных ссылок'
+                },
+                http: {
+                    label: 'HTTP Сервис',
+                    description: 'Базовые настройки HTTP сервиса',
+                    http_enabled_label: 'Активировать HTTP Сервис*',
+                    http_enabled_tooltip:
+                        'ВНИМАНИЕ: HTTP сервис необходим для функционирования виджета браузера (этой страницы). Его можно активировать ' +
+                        'только вручную изменив файл конфигурации',
+                    http_listen_addr_label: 'Прослушиваемый Адрес (host:port)',
+                    http_listen_addr_tooltip:
+                        'Какой адрес http сервис будет прослушивать. (Ссылка по которой вы сейчас подключены). Вам стоит использовать localhost' +
+                        'если вы не знаете что делаете, так как системы аутенфикации нет.'
+                },
+                steam: {
+                    label: 'Конфигурация Steam',
+                    description: 'Настройка steam api и клиента',
+                    steam_id_label: 'Steam ID',
+                    steam_id_tooltip:
+                        'Вы можете выбрать один из следующих форматов: steam,steam3,steam64',
+                    api_key_label: 'Steam API Key',
+                    api_key_tooltip: 'Ваш личный steam web api key',
+                    steam_dir_label: 'Папка Steam',
+                    steam_dir_tooltip:
+                        'Расположение вашей папки steam содержащую папку userdata'
+                },
+                tf2: {
+                    label: 'Конфигурация TF2',
+                    description: 'Изменить настройки игры',
+                    tf2_dir_label: 'Папка TF2',
+                    tf2_dir_tooltip:
+                        'Путь к вашей `steamapps/common/Team Fortress 2/tf` Папке',
+                    rcon_static_label: 'Статичный Режим RCON',
+                    rcon_static_tooltip:
+                        'Когда активирован, rcon будет всегда использовать статичные порт и пароль - 21212 / pazer_sux_lol. Иначе они будут рандомно сгенерированы при запуске игры',
+                    voice_bans_enabled_label: 'Сгенерировать Голосовые Баны',
+                    voice_bans_enabled_tooltip:
+                        'ВНИМАНИЕ: Это перепишет ваш текущий список банов. Заглушает 200 самых недавних помеченных игроков.'
+                },
+                link_editor: {
+                    create_title: `Создать Новую Ссылку`,
+                    edit_title: `Редактировать Ссылку: `,
+                    enabled_label: 'Активирован',
+                    steam_id_format: 'Формат Steam ID'
+                },
+                list_editor: {
+                    create_title: `Создать Новый Список`,
+                    edit_title: `Редактировать Список:`
                 }
             }
         }
-    }
+    },
 };
+
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
     resources,
