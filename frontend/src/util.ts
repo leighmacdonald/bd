f6db0e689339d2151abad705bbae525744832c87
Leigh MacDonald <leigh.macdonald@gmail.com>
2023-10-04 06:14:03 -0600

    Use new URL for validation

--- frontend/src/util.ts
+++ frontend/src/util.ts
@@ -39,14 +39,13 @@ export const logError = (error: unknown) => {
 };
 
 export const isValidUrl = (urlString: string): boolean => {
-    const urlPattern = new RegExp(
-        '^(https?:\\/\\/)?' + // validate protocol
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
-            '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
-            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
-            '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
-            '(\\#[-a-z\\d_]*)?$',
-        'i'
-    ); // validate fragment locator
-    return urlPattern.test(urlString);
+    try {
+        const newUrl = new URL(urlString);
+        return (
+            (newUrl.protocol === 'http:' || newUrl.protocol === 'https:') &&
+            newUrl.host != ''
+        );
+    } catch (e) {
+        return false;
+    }
 };
